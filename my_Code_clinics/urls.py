78f284640f5d80d1b2894957d96868d39790bf75
Matanga-F <matangafulufhelo@gmail.com>
2022-02-19 20:40:21 +0200

    updated templates and css

--- my_Code_clinics/urls.py
+++ my_Code_clinics/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 
 app_name = "Code Clinics"
+
 urlpatterns = [
     path("", views.index, name = "index"),
+    path("sign/", views.sign, name="sign"),
 ]
 
--- my_Code_clinics/views.py
+++ my_Code_clinics/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render (request, "index.html")
+
+def sign(request):
+    return render (request, "sign_up.html")
